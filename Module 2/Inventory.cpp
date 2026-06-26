1a20e44412bda32cc788b9eb02ebb7b624d3d3aa
TaylorSPeralta <30044798@cc.peralta.edu>
2026-06-25 21:55:56 -0700

    Add files via upload

---
+++ Module 2/Inventory.cpp
@@ -0,0 +1,13 @@
+
+
+# include <iostream>
+using namespace std;
+int main() {
+
+cout << "Hello Inventory System!\n";
+cout<<"\tData Types:\n";
+cout<<"size of int:\t"<<sizeof(int)<<"bytes\n";
+cout<<"Size of float:\t"<<sizeof(float)<<"bytes\n";
+cout<<"Size of char:\t"<<sizeof(char)<<"bytes\n";
+return 0;
+}
